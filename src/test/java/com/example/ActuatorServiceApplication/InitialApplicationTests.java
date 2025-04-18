f44f5e3aa62042f6ab91b3cdaa4d5b79ff42ffc9
GabrielC67 <gabecruzpro@gmail.com>
2025-04-18 11:26:12 -0400

    Commit

---
+++ src/test/java/com/example/ActuatorServiceApplication/InitialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ActuatorServiceApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationServiceActuatorTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
